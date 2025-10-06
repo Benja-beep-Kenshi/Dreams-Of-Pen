fc61417db7a885cc8e9c7ffa20af567bce8ae365
elinico07100 <elinico07100@gmail.com>
2025-10-06 09:15:10 -0300

    benjagay

--- app.py
+++ app.py
@@ -1,19 +1,47 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
-
-#codigo a completar
+app = Flask(__name__, template_folder="Templates", static_folder="Static")
 
 
-@app.route('/')
-
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/Reseñas')
+
+@app.route("/Reseñas")
 def reseñas():
-    return render_template('Reseñas.html')
+    return render_template("Reseñas.html")
 
-    
-if __name__ == '__main__':
+
+@app.route("/Mis reseñas")
+def misr():
+    return render_template("Mis reseñas.html")
+
+
+@app.route("/Ver Reseñas")
+def verRe():
+    return render_template("Ver Reseñas.html")
+
+
+@app.route("/crear")
+def crear():
+    return render_template("crear.html")
+
+
+@app.route("/vermias")
+def verMias():
+    return render_template("vermias.html")
+
+
+@app.route("/busquedaMarca")
+def BusM():
+    return render_template("busquedaMarca.html")
+
+
+@app.route("/busquedaUsuario")
+def BusU():
+    return render_template("busquedaUsuario.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
